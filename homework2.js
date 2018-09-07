f121bb3240354330128da67da083b3b7e55303b5
Janna <jannamanukjan@gmail.com>
2018-09-07 18:22:57 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,23 @@
+alert ("Hi");
+
+const a = 12;
+const b = 40;
+const c = 2;
+
+const w = 'smaller';
+
+
+const d = c*a/b+c-a+b; 
+const e = d/2+1*b;
+
+
+console.log(e);
+console.log(d);
+
+if (e<d){
+console.log(e +' is ' +w+ ' than '+ d);
+}
+
+else{
+	console.log(d + ' is ' +w+ ' than '+ e);
+}
